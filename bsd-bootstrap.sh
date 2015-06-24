8f237d1dd950a1877437ad507d1d1fefdd1d26b1
_NSAKEY <root [at] abigisp [dot] com>
2015-06-24 13:31:23 -0500

    Removed the ssh code, because it's out of scope.

--- bsd-bootstrap.sh
+++ bsd-bootstrap.sh
@@ -39,30 +39,6 @@ sed -ir "s/NETWORK_PLACEHOLDER/$NETWORK/g" $PWD/etc/pf.conf
 sed -ir "s/IPv4_PLACEHOLDER/$NETWORK/g" $PWD/etc/pf.conf
 sed -ir "s/IPv6_PLACEHOLDER/$NETWORK/g" $PWD/etc/pf.conf
 
-# configure sshd
-ORIG_USER=$(logname)
-if [ -n "$ORIG_USER" ]; then
-        echo "== Configuring sshd"
-        # only allow the current user to SSH in
-        echo "AllowUsers $ORIG_USER" >> /etc/ssh/sshd_config
-        echo "  - SSH login restricted to user: $ORIG_USER"
-        if grep -q "Accepted publickey for $ORIG_USER" /var/log/auth.log; then
-                # user has logged in with SSH keys so we can disable password authentication
-                sed -i '/^#\?PasswordAuthentication/c\PasswordAuthentication no' /etc/ssh/sshd_config
-                echo "  - SSH password authentication disabled"
-                if [ $ORIG_USER == "root" ]; then
-                        # user logged in as root directly (rather than using su/sudo) so make sure root login is enabled
-                        sed -i '/^#\?PermitRootLogin/c\PermitRootLogin yes' /etc/ssh/sshd_config
-                fi
-        else
-                # user logged in with a password rather than keys
-                echo "  - You do not appear to be using SSH key authentication.  You should set this up manually now."
-        fi
-        service sshd reload
-else
-        echo "== Could not configure sshd automatically.  You will need to do this manually."
-fi
-
 # final instructions
 echo ""
 echo "== Try SSHing into this server again in a new window, to confirm the firewall isn't broken"
